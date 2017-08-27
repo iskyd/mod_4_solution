bf0ba49efa7beba98289d6d2b663a997af32f1cd
iskyd <careddumattia@gmail.com>
2017-08-27 15:46:33 +0200

    edit index

--- src/menuapp/categories.controller.js
+++ src/menuapp/categories.controller.js
@@ -4,7 +4,6 @@
 angular.module('MenuApp')
 .controller('CategoriesController', CategoriesController);
 
-// Version with resolving to 1 item based on $stateParams in route config
 CategoriesController.$inject = ['categories'];
 function CategoriesController(categories) {
   var vm = this;
